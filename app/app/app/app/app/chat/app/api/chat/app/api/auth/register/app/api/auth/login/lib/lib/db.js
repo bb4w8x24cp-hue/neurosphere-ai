adb6373366dbfa3486fc81796753d16080de7f40
bb4w8x24cp-hue <bb4w8x24cp@privaterelay.appleid.com>
2025-11-30 10:56:01 +0100

    Delete app/app/app/app/app/chat/app/api/chat/app/api/auth/register/app/api/auth/login/lib/lib directory

--- app/app/app/app/app/chat/app/api/chat/app/api/auth/register/app/api/auth/login/lib/lib/db.js
+++
@@ -1,6 +0,0 @@
-import { JSONFilePreset } from "lowdb/node";
-
-// Database file location
-const defaultData = { users: [] };
-
-export const db = await JSONFilePreset("database.json", defaultData);
